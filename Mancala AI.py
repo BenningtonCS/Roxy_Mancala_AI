836007a6b3bfe488ce7d4bdfb134ae30435be558
Rose/Roxy <47798467+lonelyVoxel@users.noreply.github.com>
2021-02-24 16:59:28 -0500

    Mancala AI update
    Fixed AI acting in unexpected ways by un-nesting all the if statements. As it stands now, a game with two expert AI cannot be completed because of a hangup I haven't looked into yet.

--- Mancala AI.py
+++ Mancala AI.py
@@ -18,7 +18,7 @@ def choice_recur(player_id: int, curr_board: [int], recur_level: int) -> [int]:
 
 
 # Player functions take two parameters:
-# Player_id, 0 or 1, and board, which is the state of the board. Probably a string.
+# Player_id, 0 or 1, and board, which is the state of the board, which is an array of ints.
 # The return is the index of the pit to be played in
 
 def random_player(player_id: int, curr_board: [int]) -> int:
@@ -29,28 +29,32 @@ def random_player(player_id: int, curr_board: [int]) -> int:
 def expert_player(player_id: int, curr_board: [int]) -> int:
     scale_factor = player_id * 7
     open_spots = get_open_spots(player_id, curr_board)
-    for x in range(5 + scale_factor, -1 + scale_factor, -1):
+    # player_dish = mancala.player_score_dish (keeping in case it becomes relevant again)
+    
+    for x in range((5 + scale_factor), (-1 + scale_factor), -1):
         if curr_board[x] + x - scale_factor == 6:
             return x  # This prioritizes moves that give extra moves, starting from the pit directly next to the store
-        else:  # Here I go three levels deep of moves intended to set up moves that give extra move on subsequent turns
-            if curr_board[x] + x - scale_factor == 5:
-                return x
-            else:  # There's probably a way to make this recur but it's copy/pasted for now
-                if curr_board[x] + x - scale_factor == 4:
-                    return x
-                else:
-                    if curr_board[x] + x - scale_factor == 3:
-                        return x
-                    else:  # Here's the implementation for the opposite side capture choice
-                        for y in range(0 + scale_factor, 6 + scale_factor):
-                            if (y + curr_board[y]) % 7 < 6:
-                                if mancala.opposite_pit(y + curr_board[y]) != 0:
-                                    if curr_board[x + y] == 0:
-                                        return y
-                                    else:
-                                        return random.choice(open_spots)
+        
+        # Here I go three levels deep of moves intended to set up moves that give extra move on subsequent turns
+        if curr_board[x] + x - scale_factor == 5:
+            return x
+        
+        # There's probably a way to make this recur but it's copy/pasted for now
+        if curr_board[x] + x - scale_factor == 4:
+            return x
+
+        # if curr_board[x] + x - scale_factor == 3:
+        #    return x
+        
+    # Here's the implementation for the opposite side capture choice
+    for y in range((0 + scale_factor), (6 + scale_factor)):
+        if mancala.opposite_pit((y + curr_board[y]) % 7) != 0:
+            return y
+    
+    # Return a random choice if all else fails
+    return random.choice(open_spots)
 
 
-mancala.play_game([mancala.human_player, expert_player], should_display=True, start_player=0)
+# mancala.play_game([expert_player, mancala.human_player], should_display=True, start_player=0)
 
 mancala.run_simulations([expert_player, random_player], 10000, print_statistics=True)
